ae1e56c1312e8346c5db479554a5f7b23141f74a
Jose  Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2021-11-03 20:40:36 +0000

    More examples

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Conditionals: If Statements
+let x=5;
+if (x==5){
+  console.log(true);
+}
+console.log("end of the program");
+
+let y=5;
+if (y==10){
+  console.log(true);
+}
+console.log("end of the program");
+
+let z=20;
+if (z==5){
+  console.log('option1');
+} else {
+  console.log('option2');
+} //If [blank] is true, then do [option1], if not then, do [option2] then end of program
+console.log("end of the program");
+
+let w=20;
+if (w==20){
+  console.log('option1');
+} else {
+  console.log('option2');
+} //If [blank] is true, then do [option1], if not then, do [option2] then end of program
+console.log("end of the program");
+
+let num1=7;
+if (num1==10) {
+  console.log('option1');
+} else if (num1==7) {
+  console.log('option2');
+} else {
+  console.log('option3');
+}
+console.log('end of program');
+
+let num4=5;
+if (num4 > 10) {
+  console.log('greater than 10');
+} else if (num4 < 2) {
+  console.log("less than 2");
+}
+console.log('end of program');
+
+let secret = 49;
+if (secret % 49 == 0) {
+  console.log ('greater than 10');
+} else {
+  console.log('smaller than 10');
+}
+console.log('end of program')
